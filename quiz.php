c31a753193696679fe83a8ca6d3ce495e36976d4
BuildTools <unconfigured@null.spigotmc.org>
2023-10-02 04:12:37 +0700

    remake Quiz 1:v2

--- quiz.php
+++ quiz.php
@@ -11,39 +11,41 @@ $matriks1 = array(
 
 
 $matriks2 = array(
-    [6],
-    [7],
-    [8],
-    [9],
-    [10]
+    6,
+    7,
+    8,
+    9,
+    10
 );
 
-// foreach($matriks1 as $row => $column) {
-//     $matriks = $matriks2[$row];
-//     $output = 0;
-//     foreach($column as $data) {
-//         $kali = $data * $matriks;
-//         $output += $kali;
-//     }
-//     echo "<p>$output</p>";
-// }
-
-
-foreach ($matriks2 as $index => $row2) {
+foreach($matriks1 as $index => $row1) {
     $output = 0;
-    foreach ($row2 as $iRow2 => $valRow2) {
-        $output = 0;
-        $matriks = $matriks1[$index];
-        foreach ($matriks as $val1) {
-            $kali = $val1 * $valRow2;
+    foreach($row1 as $iRow1 => $valRow1) {
+            // echo "($valRow1 kali " . $matriks2[$iRow1] .")+";
+            $kali = $valRow1 * $matriks2[$iRow1];
             $output += $kali;
-        }
-        echo "$output  ";
     }
-
-
+    echo "$output";
     echo "<br>";
 }
+
+
+// foreach ($matriks2 as $index => $row2) {
+//     $output = 0;
+//     foreach ($row2 as $iRow2 => $valRow2) {
+//         $output = 0;
+//         $matriks = $matriks1[$index];
+//         foreach ($matriks as $val1) {
+//             echo "($val1 kali $valRow2)";
+//             $kali = $val1 * $valRow2;
+//             $output += $kali;
+//         }
+//         // echo "$output  ";
+//     }
+
+
+//     echo "<br>";
+// }
 
 // Quiz 2 
 echo "<br>";
